6ca2ec1aa74734c752c5a6b519e5a6313c135fc1
shubham-pundir0001 <shubhampundir9536@gmail.com>
2026-05-06 11:48:44 +0530

    concepts of file handling
    file handling

---
+++ theoryConcepts/filehand.py
@@ -0,0 +1,12 @@
+# f=open("abc.txt",'w')
+# f.write("shubham \n ah")
+
+# f=open("abc.txt",'r')
+# print(f.read())
+
+# f=open("abc.txt",'a')
+# f.write(" pundir")
+
+
+# f=open("abc.txt",'r')
+# print(f.readlines())
